5aabab74e0094e6fe50fa1532a9adf7ed2abc584
Maksim Kazyrytski <maksim.kazyrytski@LeverX.local>
2022-04-19 12:18:21 +0300

    [at] - implement function

--- at.js
+++ at.js
@@ -13,6 +13,37 @@
  * at(object, ['a[0].b.c', 'a[1]'])
  * // => [3, 4]
  */
-const at = (object, ...paths) => {}
+const at = (object, ...paths) => {
+  const getPathValue = (entryPaths) => entryPaths.reduce((acc, path, idx, arr) => {
+    const matchedArrayIdx = path.match(/\[(.*?\d)\]/)
+
+    if (matchedArrayIdx && acc[path[0]] && acc[path[0]][matchedArrayIdx[1]]) {
+      return acc[path[0]][matchedArrayIdx[1]]
+    }
+    if (!matchedArrayIdx && acc[path]) {
+      return acc[path]
+    }
+    arr.splice(1)
+    return
+  }, object || {})
+
+  const parsePath = (entryPaths) => entryPaths.map((path) => {
+    const exploredPath = typeof path === 'string' ? path.split('.') : [String(path)]
+    return getPathValue(exploredPath)
+  })
+
+  const isPseudoArray = !(paths[0] && paths[0].sort) && typeof paths[0] === 'object'
+  const isArray = Array.isArray(paths[0])
+
+  if (isPseudoArray) {
+    return parsePath([...paths[0]])
+  }
+
+  if (isArray) {
+    return parsePath(paths[0])
+  }
+
+  return parsePath(paths)
+}
 
 export default at
