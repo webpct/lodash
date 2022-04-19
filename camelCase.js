05a0c851807442fec3ce4a2008234b59ad72c63b
Maksim Kazyrytski <maksim.kazyrytski@LeverX.local>
2022-04-19 20:19:09 +0300

    [camelCase] - implement function

--- camelCase.js
+++ camelCase.js
@@ -17,7 +17,51 @@
  * camelCase('__FOO_BAR__')
  * // => 'fooBar'
  */
-const camelCase = (string) => {}
+const camelCase = (string) => {
+  const isSingleWord = string.match(/[a-zA-Z0-9]+/g).length === 1
+
+  const multiWords = (entryString) => entryString
+    .match(/[a-zA-Z0-9]+/g)
+    .reduce((camelCaseStr, word, idx) => {
+      if (idx === 0) {
+        return `${camelCaseStr}${word.toLocaleLowerCase()}`
+      }
+
+      if (/\D/.test(word)) {
+        const wordWithDigit = word.match(/[a-z]+|\d+/gi)
+        const digitalCamelCaseStr = wordWithDigit.reduce((digitCamelCaseStr, digitalWord) =>
+          `${digitCamelCaseStr}${digitalWord[0].toUpperCase() +
+            digitalWord.slice(1).toLocaleLowerCase()}`, '')
+
+        return `${camelCaseStr}${digitalCamelCaseStr}`
+      }
+
+      return `${camelCaseStr}${word[0].toUpperCase() + word.slice(1).toLocaleLowerCase()}`
+    }, '')
+
+  const acronymsWords = (entryString) => {
+    const acronymsCamelCase = entryString.replace(/[A-Z]+/g, (replacedString) => {
+      if (replacedString.length > 2) {
+        return replacedString
+          .split('')
+          .map((letter, idx, arr) => {
+            if (idx === 0 || idx === arr.length -1) {
+              return letter
+            }
+            return letter.toLocaleLowerCase()
+          })
+          .join('')
+      }
+      return replacedString
+    })
+
+    return `${acronymsCamelCase[0].toLocaleLowerCase() +
+        acronymsCamelCase.slice(1, acronymsCamelCase.length - 1) +
+        acronymsCamelCase[acronymsCamelCase.length - 1].toLocaleLowerCase()}`
+  }
+
+  return  isSingleWord ? acronymsWords(string) : multiWords(string)
+}
 
 export default camelCase
 
