776b56e9529b6e4e4380cb0db703775b3e6c3159
Maksim Kazyrytski <maksim.kazyrytski@LeverX.local>
2022-05-02 15:23:19 +0300

    [invoke] - implement function

--- invoke.js
+++ invoke.js
@@ -14,7 +14,41 @@
  * invoke(object, 'a[0].b.c.slice', [1, 3])
  * // => [2, 3]
  */
-function invoke(object, path, args) {
+function invoke(object, path, ...args) {
+  const getPathValue = (entryPaths) => entryPaths.reduce((acc, path, idx, arr) => {
+    const isArrayPath = path.match(/\[(.*?\d)\]/)
+    const [_, matchedArrayIdx] = isArrayPath || []
+    const [currentPathKey] = path
+
+    if (!acc) {
+      arr.splice(1)
+      return
+    }
+
+    if (isArrayPath && acc[currentPathKey] && acc[currentPathKey][matchedArrayIdx]) {
+      return acc[currentPathKey][matchedArrayIdx]
+    }
+
+    if (!isArrayPath && acc[path] && typeof acc[path] !== 'function') {
+      return acc[path]
+    }
+
+    if (!isArrayPath && acc[path] && typeof acc[path] === 'function') {
+      return {
+        iterableObj: acc,
+        func: acc[path]
+      }
+    }
+
+  }, object || {})
+
+  if (Array.isArray(path)) {
+    const { iterableObj, func } = getPathValue(path) || {}
+    return func ? func.apply(iterableObj, args) : undefined
+  }
+
+  const { iterableObj, func } = getPathValue(path.split('.')) || {}
+  return func ? func.apply(iterableObj, args) : undefined
 }
 
 export default invoke
