fcd89fe28edb164dd00ba1d299785530935c0819
Maksim Kazyrytski <maksim.kazyrytski@LeverX.local>
2022-05-02 15:23:41 +0300

    [kebabCase] - implement function

--- kebabCase.js
+++ kebabCase.js
@@ -18,6 +18,39 @@
  * kebabCase('__FOO_BAR__')
  * // => 'foo-bar'
  */
-const kebabCase = (string) => {}
+const kebabCase = (string) => {
+  const multiWords = (entryString) => entryString
+    .match(/[a-zA-Z0-9]+/g)
+    .reduce((lowerCaseStr, word, idx) => {
+      const lowerCaseWords = acronymsWords(word)
+        .split(/(?=[A-Z])/)
+        .map((acronymWord) => acronymWord.toLocaleLowerCase())
+        .join('-')
+      return idx === 0 ? `${lowerCaseStr} ${lowerCaseWords}` : `${lowerCaseStr}-${lowerCaseWords}`
+    }, '')
+
+  const acronymsWords = (entryString) => {
+    const acronymsCamelCase = entryString.replace(/[A-Z]+/g, (replacedString) => {
+      if (replacedString.length > 2) {
+        return replacedString
+          .split('')
+          .map((letter, idx, arr) => {
+            if (idx === 0 || idx === arr.length -1) {
+              return letter
+            }
+            return letter.toLocaleLowerCase()
+          })
+          .join('')
+      }
+      return replacedString
+    })
+
+    return `${acronymsCamelCase[0].toLocaleLowerCase() +
+        acronymsCamelCase.slice(1, acronymsCamelCase.length - 1) +
+        acronymsCamelCase[acronymsCamelCase.length - 1].toLocaleLowerCase()}`
+  }
+
+  return multiWords(string).trim()
+}
 
 export default kebabCase
