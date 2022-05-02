a94dc66e995321ab88a294f7327669aa867c297a
Maksim Kazyrytski <maksim.kazyrytski@LeverX.local>
2022-05-02 15:21:20 +0300

    [get] - implement function

--- get.js
+++ get.js
@@ -23,6 +23,48 @@
  * // => 'default'
  */
 function get(object, path, defaultValue) {
+  let parsedPath = []
+
+  if (typeof path === 'string') {
+    parsedPath = path.split('.')
+  }
+
+  if (Array.isArray(path)) {
+    parsedPath = path.reduce((acc, item, idx, array) => {
+      if (array.length - 1 !== idx && Number.isInteger(+array[idx + 1])) {
+        return [...acc, `${item}[${array[idx + 1]}]`]
+      }
+
+      if (Number.isInteger(+item)) {
+        return acc
+      }
+
+      return [...acc, item]
+    }, [])
+  }
+
+  const getPathValue = (entryPaths) => entryPaths.reduce((acc, path, idx, arr) => {
+    const isArrayPath = path.match(/\[(.*?\d)\]/)
+    const [_, matchedArrayIdx] = isArrayPath || []
+    const [currentPathKey] = path
+
+    if (!acc) {
+      arr.splice(1)
+      return
+    }
+
+    if (isArrayPath && acc[currentPathKey] && typeof acc[currentPathKey][matchedArrayIdx] !== 'undefined') {
+      return acc[currentPathKey][matchedArrayIdx]
+    }
+
+    if (!isArrayPath && typeof acc[path] !== 'undefined') {
+      return acc[path]
+    }
+  }, object || {})
+
+  const value = getPathValue(parsedPath)
+
+  return typeof value === 'undefined' ? defaultValue : value
 }
 
 export default get
